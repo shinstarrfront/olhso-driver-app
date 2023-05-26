c12ef1949ae367e060fe11db270733dd4a1acb8c
shinstarrfront <https://github.com/shinstarrfront>
2023-05-26 11:17:19 +0900

    feat: Change the password 유효성 검사 및 ui 적용 완료

--- src/srceens/ChangePasswordScreen.tsx
+++ src/srceens/ChangePasswordScreen.tsx
@@ -1,98 +1,3 @@
-// import * as React from 'react';
-// import { useContext, useState } from 'react';
-// import { StyleSheet, Text, View, TouchableOpacity, TextInput } from "react-native";
-// import { SignInContext } from '../contexts/SignInContext';
-// import { Auth } from 'aws-amplify'
-// import { CognitoUser } from 'amazon-cognito-identity-js';
-// import { RouteProp } from '@react-navigation/native';
-// import { StackNavigationProp } from '@react-navigation/stack';
-// import { RootStackParamList } from '../types';
-
-
-
-// interface ChangePasswordScreenRouteProp extends RouteProp<RootStackParamList, 'ChangePassword'> {}
-// interface ChangePasswordScreenNavigationProp extends StackNavigationProp<RootStackParamList, 'ChangePassword'> {}
-
-// interface ChangePasswordScreenProps {
-//   route: ChangePasswordScreenRouteProp;
-//   navigation: ChangePasswordScreenNavigationProp;
-// }
-
-// const ChangePasswordScreen: React.FunctionComponent<ChangePasswordScreenProps> = ({navigation, route}) => {
-//     const [newPassword, setNewPassword] = useState('');
-//     const [confirm, setConfirm] = useState('');
-//     const [maskedConfirm, setMaskedConfirm] = useState('');
-//     const [showPasswordCheckNo, setShowPasswordCheckNo] = useState(false);
-//     const [showPasswordCheckOk, setShowPasswordCheckOk] = useState(false);
-
-//   const { user } = route.params; // 전달 받기
-
-//   const handlePasswordChange = (value: string) => {
-//     setNewPassword(value);
-//   };
-
-//   const handleConfirmChange = (value: string) => {
-//     const maskedValue = value.replace(/./g, '*');
-//     setConfirm(value);
-//     setMaskedConfirm(maskedValue);
-  
-//     if (newPassword === '') {
-//       setShowPasswordCheckNo(false);
-//       setShowPasswordCheckOk(false);
-//     } else if (value === '') {
-//       setShowPasswordCheckNo(false);
-//       setShowPasswordCheckOk(false);
-//     } else if (newPassword !== value) {
-//       setShowPasswordCheckNo(true);
-//       setShowPasswordCheckOk(false);
-//     } else {
-//       setShowPasswordCheckNo(false);
-//       setShowPasswordCheckOk(true);
-//     }
-//   };
-  
-
-//   const changePassword = async () => {
-//     try {
-//       if (newPassword !== confirm) {
-//         alert('Passwords do not match.');
-//         return;
-//       }
-//       await Auth.completeNewPassword(user, newPassword);
-//       console.log('Password changed successfully');
-//     } catch (error) {
-//       console.log('Error changing password:', error);
-//     }
-//   };
-  
- 
-  
-//   return (
-//     <View style={styles.container}>
-//       <Text style={styles.title}>Change your password</Text>
-//       <TouchableOpacity style={styles.password}>
-//         <TextInput style={styles.passwordplaceholder} 
-//         placeholder="Enter the new Password"  
-//         defaultValue={newPassword} 
-//         onChangeText={handlePasswordChange}/>
-//       </TouchableOpacity>
-//       <TouchableOpacity style={styles.confirmpassword}>
-//       <TextInput 
-//         style={styles.confirmpasswordplaceholder} 
-//         placeholder="Confirm your Password"  
-//         value={maskedConfirm} 
-//         onChangeText={handleConfirmChange}
-//       />
-//       </TouchableOpacity>
-//       {showPasswordCheckNo && <Text style={styles.passwordcheckno}>Password doesn't match</Text>} 
-//       {showPasswordCheckOk && <Text style={styles.passwordcheckok}>The passwords match!</Text>}
-//       <TouchableOpacity style={styles.changedbtn} onPress={changePassword} disabled={showPasswordCheckNo} >
-//         <Text style={styles.changedbtnfont}>Changed</Text>
-//       </TouchableOpacity>
-//     </View>
-//   );
-// };
-
 import * as React from 'react';
 import { useContext, useState } from 'react';
 import { StyleSheet, Text, View, TouchableOpacity, TextInput } from "react-native";
@@ -120,13 +25,22 @@ const ChangePasswordScreen: React.FunctionComponent<ChangePasswordScreenProps> =
   const [showPasswordCheckNo, setShowPasswordCheckNo] = useState(false);
   const [showPasswordCheckOk, setShowPasswordCheckOk] = useState(false);
   const [isLoading, setIsLoading] = useState(false);
+  const [isPasswordValid, setIsPasswordValid] = useState(false);
+  const [hasLowerCase, setHasLowerCase] = useState(false);
+  const [hasUpperCase, setHasUpperCase] = useState(false);
+  const [hasNumber, setHasNumber] = useState(false);
+  const [hasSpecialChar, setHasSpecialChar] = useState(false);
 
   const { user } = route.params; // 전달 받기
 
   const handlePasswordChange = (value: string) => {
     setNewPassword(value);
+    setIsPasswordValid(value.length >= 8);
+    setHasLowerCase(/[a-z]/.test(value));
+    setHasUpperCase(/[A-Z]/.test(value));
+    setHasNumber(/[0-9]/.test(value));
+    setHasSpecialChar(/[!@#$%^&*()]/.test(value));
     if (confirm !== '' && value !== confirm) {
-      // 새 비밀번호 입력 후 confirm값이 있고 두 값이 일치하지 않는 경우
       setShowPasswordCheckNo(true);
       setShowPasswordCheckOk(false);
     } else {
@@ -134,6 +48,7 @@ const ChangePasswordScreen: React.FunctionComponent<ChangePasswordScreenProps> =
       setShowPasswordCheckOk(false);
     }
   };
+
 
   const handleConfirmChange = (value: string) => {
     setConfirm(value);
@@ -183,10 +98,12 @@ const changePassword = async () => {
        <View style={styles.boxcolumn}> 
        <View style={styles.box}>
       <TouchableOpacity style={styles.password}>
-        <TextInput style={styles.inputpassword} 
-        placeholder="Enter the new Password"  
-        defaultValue={newPassword} 
-        onChangeText={handlePasswordChange}/>
+        <TextInput 
+        style={styles.inputpassword} 
+        placeholder="Enter the new Password"
+        defaultValue={newPassword}
+        onChangeText={handlePasswordChange}
+     />
       </TouchableOpacity>
       <TouchableOpacity style={styles.confirmpassword}>
       <TextInput 
@@ -198,13 +115,18 @@ const changePassword = async () => {
       </TouchableOpacity>
       </View>
       <View style={styles.box2}>
-      <Text style={styles.testpassword1}>At least 8 characters</Text>
-      <Text style={styles.testpassword2}>One lower case letter</Text>
-      <Text style={styles.testpassword3}>One upper case letter</Text>
-      <Text style={styles.testpassword4}>One number</Text>
-      <Text style={styles.testpassword5}>One special character</Text>
-      {showPasswordCheckNo && <Text style={styles.testpasswordcheckno}>Password doesn't match</Text>}
-      {showPasswordCheckOk && <Text style={styles.testpasswordcheckok}>The passwords match!</Text>}
+      <Text style={[styles.testpassword1, isPasswordValid && { color:  'black' }]}>
+         <Text style={{ color: isPasswordValid ? '#ED6A2C' : '#FFFFFF' }}>✓</Text> At least 8 characters</Text>
+      <Text style={[styles.testpassword2, hasLowerCase && { color: 'black' }]}>
+      <Text style={{ color: hasLowerCase ? '#ED6A2C' : '#FFFFFF' }}>✓</Text> One lower case letter</Text>
+      <Text style={[styles.testpassword3, hasUpperCase && { color: 'black' }]}>
+      <Text style={{ color: hasUpperCase ? '#ED6A2C' : '#FFFFFF' }}>✓</Text> One upper case letter</Text>
+      <Text style={[styles.testpassword4, hasNumber && { color: 'black' }]}>
+      <Text style={{ color: hasNumber ? '#ED6A2C' : '#FFFFFF' }}>✓</Text> One number</Text>
+      <Text style={[styles.testpassword5, hasSpecialChar && { color: 'black' }]}>
+      <Text style={{ color: hasSpecialChar ? '#ED6A2C' : '#FFFFFF' }}>✓</Text> One special character</Text>
+      {showPasswordCheckNo && <Text style={styles.testpasswordcheckno}>✕ Password doesn't match</Text>}
+      {showPasswordCheckOk && <Text style={styles.testpasswordcheckok}>✓ The passwords match</Text>}
       </View>
       <View style={styles.box3}>
       <TouchableOpacity style={styles.changedbtn} onPress={changePassword} disabled={showPasswordCheckNo} >
@@ -238,7 +160,7 @@ const styles = StyleSheet.create({
       width: '100%',
       heigth: 300,
       top: 157,
-      left: 20
+      // left: 20
     },
     box3: {
       position: 'absolute',
@@ -323,12 +245,12 @@ const styles = StyleSheet.create({
     },
     testpasswordcheckno:{
       marginTop: 12,
-      color: '#ED6A2C',
+      color: '#838796',
       fontSize: 13,
     },
     testpasswordcheckok: {
       marginTop: 12,
-      color: 'green',
+      color: '#ED6A2C',
       fontSize: 13,
     }
 });
